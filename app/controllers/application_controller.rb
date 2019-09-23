5d539b5a067c7b6f6f3e3e8f4311b42dd87b7b47
shotaro yuki <yuki8118tmds@gmail.com>
2019-09-23 12:01:59 +0900

    ログアウトユーザーアクセス制限2

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,23 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include SessionsHelper
-  before_action :login_required
 
   private
 
-  def login_required
-    redirect_to new_session_path unless current_user
+  #ログインユーザーと編集しようとしているユーザーのidが一致しない場合にアクセスを拒否
+  # def ensure_correct_user
+  #   if @current_user.id != params[:id].to_i
+  #     flash[:notice] = "権限がありません"
+  #     redirect_to new_session_path
+  #   end
+  # end
+
+  #ログアウト状態の時アクセス拒否
+  def authenticate_user
+    if current_user == nil
+      flash[:notice] = "ログインが必要です"
+      redirect_to new_session_path
+    end
   end
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user, only: [:index, :edit, :update, :destroy]
+
   def index
     @users = User.all
   end
