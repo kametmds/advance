a9ef2eddd236b4234af4582b04d88a749c298af1
shotaro yuki <yuki8118tmds@gmail.com>
2019-09-24 12:01:55 +0900

    mailer機能heroku導入

--- app/mailers/feed_mailer.rb
+++ app/mailers/feed_mailer.rb
@@ -1,6 +1,6 @@
 class FeedMailer < ApplicationMailer
   def feed_mail(feed)
     @feed = feed
-    mail to: "yuki8118tmds@gmail.com", subject: "投稿確認メール"
+    mail to: @feed.user.email, subject: "投稿確認メール"
   end
 end
