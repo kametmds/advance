3685d5845871ee631f7037ae2df3bf8002d35fe3
shotaro yuki <yuki8118tmds@gmail.com>
2019-09-27 11:58:26 +0900

    会員登録後エラー修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -24,7 +24,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to user_path(@user.id)
+      redirect_to new_session_path
       flash[:notice] = 'ユーザー登録しました'
     else
       render 'new'
